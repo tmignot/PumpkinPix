a072a52a2903ac5a402cd7662bc401034cd82d4b
dyarob <dyarob@gmail.com>
2015-10-05 07:19:32 +0200

    bmpfileheader gets read in hexagon

--- hexagon.c
+++ hexagon.c
@@ -13,7 +13,9 @@
 
 int	pphexagon(int ac, char** av) {
 	int	fd;
-	char	magic[2];
+	unsigned char	magic[2];
+	unsigned char	bmpfileheader[12];
+	unsigned char*	bmpinfoheader;
 
 	if (ac == 1) {
 		puts("hexagon: usage: pp --hexagon bmpfilename");
@@ -28,7 +30,11 @@ int	pphexagon(int ac, char** av) {
 	if (magic[0] != 'B' || magic[1] != 'M') {
 		printf("hexagon: %s: not a bmp file\n", av[1]);}
 	else {
-		printf("hexagon: %s: bmp header\n", av[1]);
+		printf("hexagon: %s: BMPFILEHEADER\n", av[1]);
+		read(fd, (void*)bmpfileheader, 12);
+		printf("%3d %3d %3d %3d |%10i  file size\n", bmpfileheader[0], bmpfileheader[1], bmpfileheader[2], bmpfileheader[3], ((int*)bmpfileheader)[0]);
+		printf("%3d %3d %3d %3d |%10i  reserved space\n", bmpfileheader[4], bmpfileheader[5], bmpfileheader[6], bmpfileheader[7], ((int*)bmpfileheader)[1]);
+		printf("%3d %3d %3d %3d |%10i  bmp header total size\n", bmpfileheader[8], bmpfileheader[9], bmpfileheader[10], bmpfileheader[11], ((int*)bmpfileheader)[2]);
 	}
 
 	close(fd);
